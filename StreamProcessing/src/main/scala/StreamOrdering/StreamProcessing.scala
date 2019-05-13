fe1b3f9b0c43a495a3ded3036829a934ae29d24e
Amey Ruikar <ruikar@usc.edu>
2019-05-12 17:25:23 -0700

    updated jar(2.3)

--- StreamProcessing/src/main/scala/StreamOrdering/StreamProcessing.scala
+++ StreamProcessing/src/main/scala/StreamOrdering/StreamProcessing.scala
@@ -32,13 +32,52 @@ object StreamProcessing {
 
     val recByOrderId = recordsRDD.map(parseRecords)
     recByOrderId.print()
+
+    val readyOrders = recByOrderId.mapWithState(
+      StateSpec.function(mappingFunc _)
+    ).filter(x => x._2 != true)
     
+    readyOrders.print()
+
     ssc.start()
     ssc.awaitTermination()
   }
 
-  def parseRecords(rec: String):(String, (String, String, String)) = {
+  def mappingFunc(key: String, newItem: Option[String], state: State[(Boolean, Boolean)]): (String, Boolean) = {
+
+      val item = newItem.getOrElse(null)
+            if(item == null) {
+              return (key, false)
+            }
+            val prevState = state.get()
+
+            if(prevState == (false, false)) {
+              // state does not exist
+              if (item.equals("fish")) {
+                state.update((true, false))
+              }
+              else {
+                state.update((false, true))
+              }
+              return (key, false)
+
+            }
+            else {
+              // state exists
+              if((item.equals("fish") && prevState == (false, true) ) || (item.equals("chips") && prevState == (true, false)) ) {
+                state.remove()
+                return (key, true)
+              }
+              else {
+                  return (key, false)
+              }
+
+            }
+
+  }
+
+  def parseRecords(rec: String):(String, String) = {
     val parts = rec.split("""\|""")
-    return (parts(1), (parts(0), parts(1), parts(2)))
+    return (parts(1), parts(2))
   }
 }
